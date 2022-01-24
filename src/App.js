b66ab1507d0e75ffb3408ba30713f3dcb3e076d1
goo-gy <lkukh17@naver.com>
2022-01-24 15:32:04 +0900

    make a hook (useTabs)

--- src/App.js
+++ src/App.js
@@ -13,16 +13,52 @@ const useInput = (initialValue, validator) => {
   return { value, onChange };
 };
 
+const useTabs = (initialIndex, tabs) => {
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  if (!Array.isArray(tabs)) {
+    return;
+  }
+  return { item: tabs[tabIndex], changeItem: (index) => setTabIndex(index) };
+};
+
+const allTabs = [
+  {
+    name: 'Section1',
+    content: 'This is the content of the Section 1',
+  },
+  {
+    name: 'Section2',
+    content: 'This is the content of the Section 2',
+  },
+];
+
 function App() {
   const name = useInput('Default', (value) => {
     return value.length <= 30;
   });
+  const currentTab = useTabs(0, allTabs);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>{name.value}</h2>
-      <input {...name}></input>
+      <h1>Hello React Hooks</h1>
+      <div>
+        <h2>{name.value}</h2>
+        <input {...name}></input>
+      </div>
+      <hr />
+      <div>
+        {allTabs.map((tab, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              currentTab.changeItem(index);
+            }}
+          >
+            {tab.name}
+          </button>
+        ))}
+      </div>
+      <div>{currentTab.item.content}</div>
     </div>
   );
 }
